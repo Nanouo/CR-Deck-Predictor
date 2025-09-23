eb0f7edc196b4bef4772be8f70d868a2e0e207b2
Chris Wu <csw5495@mavs.uta.edu>
2025-09-23 15:46:26 -0500

    got top players

--- server/routes/topPlayersRoutes.js
+++ server/routes/topPlayersRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const router = express.Router();
 
-const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjJiMTQyMDRiLThjMzAtNGZiNy05YzA5LTNiZWViNGI1YWJjOSIsImlhdCI6MTc1ODYwMTAwMywic3ViIjoiZGV2ZWxvcGVyLzczZDIxZjQ1LWIzNGYtMTVhNS1kZDlkLWVjNTVlOThjZDc3YSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI0Ny4xODguMjIyLjE5IiwiNzYuNzguMTY5LjE0Il0sInR5cGUiOiJjbGllbnQifV19.7L1mFY2gUwt19jPDO00nSkFgddGg-1SwYcB8B3VsFVlKhBWKDL3meJyh779Itzz3fX3j6_GIVEJHEUsQM9oJ5g';
+const token = process.env.CR_API_TOKEN;;
 
 router.get('/season-top', async (req, res) => {
   try {
